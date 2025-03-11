c2f147a05d7967de7a4b7aa7cfec6d3a1c3dacb0
Trevor Karuma <trevormukundi@gmail.com>
2025-03-11 19:38:47 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+ }
+ 
+
+ saturdayFun(); 
+ saturdayFun("bathe my dog")
+ 
+ 
+ const mondayWork = function (activity = 'go to the office'){
+     return `This Monday, I will ${activity}.`;
+ }
+ 
+ 
+ mondayWork(); 
+ mondayWork("work from home");
+ 
+ function wrapAdjective(symbol) {
+     return function(adjective) {
+         return `You are ${symbol}${adjective}${symbol}!`;
+     };
+ }
+ 
+ 
+ let result1 = wrapAdjective('*');
+ console.log(result1("a hard worker")); 
+ 
+ let result2 = wrapAdjective('||');
+ console.log(result2("a dedicated programmer")); 
